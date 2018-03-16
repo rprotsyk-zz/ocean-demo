fe10f3508326ee5804d3cfc5848bc7c3ed7338e4
Roman Protsyk <rprotsyk@gmail.com>
2018-03-16 17:49:56 +0200

    Changed messages

--- src/app/ocean/ocean-twitter/ocean-twitter.component.ts
+++ src/app/ocean/ocean-twitter/ocean-twitter.component.ts
@@ -71,14 +71,14 @@ export class OceanTwitterComponent implements OnInit {
           this.messageImage = '';
         } else {
           if (profile.status[0] === 'User is not found') {
-            this.message = 'Hmmm.. it seems there is no account with this name';
-            this.messageHint = 'Please check if it’s correct and enter again';
+            this.message = 'Hmmm... we could not find account with such name';
+            this.messageHint = 'Please verify Twitter handle or try another one';
             this.messageImage = 'try-no-account';
           }
           if (profile.status[0] === 'Not enought data to extract OCEAN' ||
               profile.status[0] === 'Provided user has no self-written tweets') {
-            this.message = 'It seems it’s not enough text to analyze right now. ';
-            this.messageHint = 'We could try to check another account';
+            this.message = 'It seems there is not enough data to analyze the profile.';
+            this.messageHint = 'We could try checking another account';
             this.messageImage = 'try-not-enough-info';
           }
         }
